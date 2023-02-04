bf71f0f48a4ce89fa99ed29a4fb09e387f4a12ea
Matthew Sainsbury-Dale <msdale@uow.edu.au>
2023-02-04 22:18:42 +1100

    codecov badge

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 [![][docs-dev-img]][docs-dev-url]
 [![CI](https://github.com/msainsburydale/NeuralEstimators.jl/actions/workflows/CI.yml/badge.svg)](https://github.com/msainsburydale/NeuralEstimators.jl/actions/workflows/CI.yml)
-[![Coverage](https://codecov.io/gh/msainsburydale/NeuralEstimators.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/msainsburydale/NeuralEstimators.jl)
-
-
+[![codecov](https://codecov.io/gh/msainsburydale/NeuralEstimators.jl/branch/main/graph/badge.svg?token=6cXItEsKs5)](https://codecov.io/gh/msainsburydale/NeuralEstimators.jl)
 
 
 
--- src/NeuralEstimators.jl
+++ src/NeuralEstimators.jl
@@ -63,15 +63,9 @@ include("UtilityFunctions.jl")
 end
 
 # TODO
-# - Add code coverage widget
-# - Cancel TravisCI
-# - Remove docs/build/ from the git history
 # - Simplify the univariate Gaussian example
 # - plotrisk and plotdistribution. Should be able to translate the code from R,
 #   so do this once the R interface is finished.
-# - Do this: https://documenter.juliadocs.org/stable/man/hosting/#gh-pages-Branch
-
-
 
 # ---- once I've made the project public:
 # - Add NeuralEstimators.jl to the list of packages that use Documenter: see https://documenter.juliadocs.org/stable/man/examples/
@@ -81,5 +75,5 @@ end
 # - Get DeepSetExpert working optimally on the GPU (leaving this for now as we don't need it for the paper).
 # - See if DeepSet.jl is something that the Flux people would like to include. (They may also improve the code.)
 # - With the fixed parameters method of train, there seems to be substantial overhead with my current implementation of simulation on the fly. When epochs_per_Z_refresh = 1, the run-time increases by a factor of 4 for the Gaussian process with nu varied and with m = 1. For now, I’ve added an argument simulate_on_the_fly::Bool, which allows us not to switch off on-the-fly simulation even when epochs_per_Z_refresh = 1. However, it would be good to reduce this overhead.
-# - Callback function for plotting during training! See https://www.youtube.com/watch?v=ObYDHi_jJXk&ab_channel=TheJuliaProgrammingLanguage. Also, I know there is a specific module for call backs while training Flux models, so may this is already possible in Julia too. In either case, I think train() should have an additional argument, callback. See also the example at: https://github.com/stefan-m-lenz/JuliaConnectoR.
+# - Callback function for plotting during training. See https://www.youtube.com/watch?v=ObYDHi_jJXk&ab_channel=TheJuliaProgrammingLanguage. Also, I know there is a specific module for call backs while training Flux models, so may this is already possible in Julia too. In either case, I think train() should have an additional argument, callback. See also the example at: https://github.com/stefan-m-lenz/JuliaConnectoR.
 # - Frameworks based on Neyman inversion allow for confidence sets with correct conditional coverage.
