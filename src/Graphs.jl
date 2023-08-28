c404d036df0801cdfc4ede7aa82ffd936d4dfa43
Matthew Sainsbury-Dale <msdale@uow.edu.au>
2023-08-28 12:02:58 +1000

    Minor bug fix

--- src/Graphs.jl
+++ src/Graphs.jl
@@ -549,7 +549,7 @@ end
 # the standard methods defined in Estimators.jl, but also pass through m.
 #TODO unit testing for these methods
 (est::PointEstimator{<:GNN})(g::GNNGraph, m::AbstractVector{I}) where {I <: Integer} = est.arch(g, m)
-(est::IntervalEstimator{<:GNN})(g::GNNGraph, m::AbstractVector{I}) where {I <: Integer} = vcat(est.l(Z, m), est.l(Z, m) .+ exp.(est.u(Z, m)))
+(est::IntervalEstimator{<:GNN})(g::GNNGraph, m::AbstractVector{I}) where {I <: Integer} = vcat(est.l(g, m), est.l(g, m) .+ exp.(est.u(g, m)))
 
 
 # ---- PropagateReadout ----
