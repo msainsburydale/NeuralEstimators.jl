adc9ab24ef5bbdc0c42ffaf422cc722c0b608412
Matthew Sainsbury-Dale <msdale@uow.edu.au>
2024-09-01 17:55:33 +1000

    Added file for deprecated functions

---
+++ src/deprecated.jl
@@ -0,0 +1,8 @@
+#NB deprecated because it isn't the recommended way of storing models anymore
+"""
+	loadbestweights(path::String)
+
+Returns the weights of the neural network saved as 'best_network.bson' in the given `path`.
+"""
+loadbestweights(path::String) = loadweights(joinpath(path, "best_network.bson"))
+loadweights(path::String) = load(path, @__MODULE__)[:weights]
